ac35a151a2df63737efe277c83cf75c2298de71a
Megax Lockwood <146300902+MegaxLockwood19@users.noreply.github.com>
2023-10-04 20:17:14 -0500

    Add files via upload

---
+++ GDC.py
@@ -0,0 +1,14 @@
+
+
+
+import random
+
+
+caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+longitud = int(input("por favor ingrese la contraseña:"))
+password = ""
+for i in range(longitud):
+    a = random.choice(seq= caracteres)
+
+    password = password + a 
+print(password)
